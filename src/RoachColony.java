997de2bf5bfe0c9231649ae2d8f561d5ee0820d7
KingdomCross <alexchheng1998@yahoo.com>
2019-11-23 14:34:57 -0800

    Basic Requirements

---
+++ src/RoachColony.java
@@ -0,0 +1,7 @@
+public class RoachColony {
+    public RoachColony(String first_colony, int i, double i1) {
+    } //Customer
+
+    public void party() {
+    }
+}
---
+++ src/RoachMotel.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class RoachMotel {
+    public static RoachMotel getInstance() {
+        return null;
+    }
+
+    public void createRooms() {
+    }
+
+    public MotelRoom checkIn(RoachColony rc1, String suite, ArrayList amenities) {
+        return null;
+    }
+
+    public Double checkOut(MotelRoom r2, int i, String masterRoach) {
+        return null;
+    }
+
+    public void cleanRooms() {
+    }
+}
