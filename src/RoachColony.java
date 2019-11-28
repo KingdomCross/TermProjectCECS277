b4fc0d1712394c2949074bc8d13add2715876048
CrabBucket <tom@mcratracing.com>
2019-11-27 17:00:28 -0800

    Added some private fields to roachcolony and added constructor.

--- src/RoachColony.java
+++ src/RoachColony.java
@@ -1,5 +1,13 @@
 public class RoachColony {
-    public RoachColony(String first_colony, int i, double i1) {
+	private String name;
+	private int init_pop;
+	private double growth;
+	
+	
+    public RoachColony(String name, int init_pop, double growth) {
+    	this.name = name;
+    	this.init_pop = init_pop;
+    	this.growth = growth;
     } //Customer
 
     public void party() {
