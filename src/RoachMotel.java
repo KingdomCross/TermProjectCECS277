7d6ed1dc95d073e1c3543d84460ee9055ab78555
alexyung <alexander.dung@student.csulb.edu>
2019-11-28 00:07:08 -0800

    Added methods to RoachMotel
    Defined Singleton pattern, with pseudo-default-constructor variant of
    getInstance().

--- src/RoachMotel.java
+++ src/RoachMotel.java
@@ -1,15 +1,42 @@
 import java.util.ArrayList;
 
+/**
+ * Represents a Roach Motel with a certain number of rooms and maids.
+ * Has methods to 
+ * @author Alexander Dung
+ *
+ */
 public class RoachMotel {
+	private static RoachMotel singleMotel;
+	private MotelRoom[] rooms;
+	//private Maid[] maids;
+	
+	private RoachMotel() {
+		rooms = new MotelRoom[10];
+		//maids = new Maid[10];
+	}
+	
+	private RoachMotel(int numRooms) {
+		rooms = new MotelRoom[numRooms];
+		//maids = new Maid[numMaids];
+	}
+	
     public static RoachMotel getInstance() {
-        return null;
+    	if(singleMotel == null) {
+    		singleMotel = new RoachMotel();
+    	}
+    	return singleMotel;
+    }
+    
+    public static RoachMotel getInstance(int numRooms) {
+    	if(singleMotel == null) {
+    		singleMotel = new RoachMotel(numRooms);
+    	}
+    	return singleMotel;
     }
 
-    public void createRooms() {
-    }
-
-    public MotelRoom checkIn(RoachColony rc1, String suite, ArrayList amenities) {
-        return null;
+    public MotelRoom checkIn(RoachColony colony, String suite, ArrayList amenities) {
+        
     }
 
     public Double checkOut(MotelRoom r2, int i, String masterRoach) {
@@ -17,5 +44,6 @@ public class RoachMotel {
     }
 
     public void cleanRooms() {
+    	
     }
 }
